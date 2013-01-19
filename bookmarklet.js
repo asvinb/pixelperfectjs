3b4cd7dfcb39a527baa8d914c29e0ee25d1793ad
Asvin Balloo <asvin.balloo@gmail.com>
2013-01-19 12:38:19 +0400

    Renamed bookmarklet

--- bookmarklet.js
+++
@@ -1,8 +0,0 @@
-javascript: (function () {
-	var jsCode = document.createElement('script');
-	jsCode.setAttribute('src', '//dl.dropbox.com/u/2369055/pixelperfectjs/_ui/js/pixelPerfect.all.js');
-    document.body.appendChild(jsCode);
-}());
-
-// url encoded version below
-//javascript:(function%20()%20%7B%0A%09var%20jsCode%20%3D%20document.createElement('script')%3B%0A%09jsCode.setAttribute('src'%2C%20'%2F%2Fdl.dropbox.com%2Fu%2F2369055%2Fpixelperfectjs%2F_ui%2Fjs%2FpixelPerfect.all.js')%3B%0A%20%20%20%20document.body.appendChild(jsCode)%3B%0A%7D())%3B
